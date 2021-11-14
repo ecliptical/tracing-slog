088fa050b795e4151893fc596917a84279278d0d
Peter Nehrer <pnehrer@eclipticalsoftware.com>
2021-11-14 14:50:27 -0700

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-//! Adapters for connecting structured log records from the `slog` crate into the `tracing` ecosystem.
+//! Adapters for connecting structured log records from the [slog] crate into the [tracing](https://github.com/tokio-rs/tracing) ecosystem.
+//!
+//! Use when a library uses `slog` but your application uses `tracing`.
+//!
+//! Heavily inspired by [tracing-log](https://github.com/tokio-rs/tracing/tree/5fdbcbf61da27ec3e600678121d8c00d2b9b5cb1/tracing-log).
 
 use lazy_static::lazy_static;
 
